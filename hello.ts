90332b465649d68b2e01a1220f90c97e0703bfc8
Freewind <nowindlee@gmail.com>
2020-12-17 14:31:25 +0800

    add getter

--- hello.ts
+++ hello.ts
@@ -6,18 +6,22 @@ class State {
 
   constructor(public name: string, public age: number) {
   }
+
+  get allInfo(): string {
+    return `Name=${this.name}, Age=${this.age}`;
+  }
 }
 
 const originalState: State[] = [
   new State('immer', 1)
 ];
 
-console.log('### originalState before change', originalState);
+console.log('### originalState before change', originalState, originalState.map(it => it.allInfo));
 
 const newState = produce(originalState, draft => {
   draft[0].age = 66;
   draft.push(new State('new-name', 100));
 })
 
-console.log('### originalState should not change:', originalState);
-console.log('### newState', newState);
+console.log('### originalState should not change:', originalState, originalState.map(it => it.allInfo));
+console.log('### newState', newState, newState.map(it => it.allInfo));
